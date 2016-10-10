26a91ba8e2334860072a6b9dc3217c514e957210
Unknown <robinsabatier@yahoo.fr>
2016-10-10 10:59:12 +0200

    Part 2

--- src/app.js
+++ src/app.js
@@ -10,4 +10,6 @@ http.createServer(function(req, res){
     res.end('Hello ' + id);
   });
   // Start the server
-}).listen(1337, '127.0.0.1');
+}).listen(1337, '127.0.0.1', function(){
+  console.log("running on 127.0.0.1:1337");
+});
---
+++ test/user.js
@@ -0,0 +1,16 @@
+var should = require('should');
+var users = require('../src/users.js');
+
+describe('my first test list', function() {
+  it('should get a user w/ right parameters', function(done) {
+    // do smth user
+    users.get("Robin", function(res) {
+      res.should.equal("Robin");
+      done();;
+    })
+  })
+
+  //
+
+  //...
+})
